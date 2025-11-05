38ba942c3149af77d5a4cc0cf7d8164a674892c8
SimiDaniels <simidaniels0506@gmail.com>
2025-11-05 13:02:42 +0100

    revalidate

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -5,6 +5,7 @@
 import { useSearchParams } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default function ConfirmationPage() {
   const searchParams = useSearchParams();
